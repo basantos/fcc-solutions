16621f77b86e54803c31d45ebedf0802f9f0cbc2
Brigitte Santos <santosbrigitte@gmail.com>
2018-01-12 12:28:16 -0800

    Add version 2 of solution

--- basic-algorithm-scripting/factorialize-a-number.js
+++ basic-algorithm-scripting/factorialize-a-number.js
@@ -1,4 +1,21 @@
 // Return the factorial of the provided integer
+
+// Solution v2: recursion
+// Date: 1/12/2018
+function factorialize(num) {
+  if(num === 0){
+    return 1;
+  } 
+  else {
+    return num * factorialize(num-1);
+  }
+}
+
+factorialize(5);
+
+
+// Solution v1: while loop
+// Date: 1/12/2018
 function factorialize(num) {
   if(num === 0){
     return 1;
