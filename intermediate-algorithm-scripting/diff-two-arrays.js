06dcd3ad57617af4b9079338b1fb27ce88726b75
Brigitte Santos <santosbrigitte@gmail.com>
2018-01-22 22:51:33 -0800

    Add second solution

--- intermediate-algorithm-scripting/diff-two-arrays.js
+++ intermediate-algorithm-scripting/diff-two-arrays.js
@@ -1,4 +1,21 @@
 // Find the symmetric difference of the two arrays
+
+// Solution v2: merge arrays, copy array, and single filter
+// Date: 1/22/2018
+function diffArray(arr1, arr2) {
+  var mergedArrays = arr1.concat(arr2);
+  var copy = mergedArrays;
+  return mergedArrays.filter((num,i) => {
+    var restOfArray = copy.slice(0,i).concat(copy.slice(i+1));
+    return !restOfArray.includes(num);
+  });
+}
+
+diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
+
+
+// Solution v1: double filter
+// Date: 1/22/2018
 function diffArray(arr1, arr2) {
   var newArr = [];
   var arr1Copy = arr1;
