a579f97db7272e14a872168e1c513dec7f0071e9
Brigitte Santos <santosbrigitte@gmail.com>
2018-01-29 08:26:10 -0800

    Add second solution

--- intermediate-algorithm-scripting/convert-html-entities.js
+++ intermediate-algorithm-scripting/convert-html-entities.js
@@ -1,5 +1,25 @@
 // Convert the characters &, <, >, " (double quote), and ' (apostrophe), 
 // in a string to their corresponding HTML entities.
+
+//Version 2: map
+// Date: 1/29/2018
+function convertHTML(str) {
+  let pairs = {
+    '&': '&amp;',
+    '<': '&lt;',
+    '>': '&gt;',
+    '"': '&quot;',
+    "'": '&apos;'
+  };
+  let targets = Object.keys(pairs);
+  return str.split('').map(item => {
+    if(targets.includes(item)) return pairs[item];
+    return item;
+  }).join('');
+}
+
+// Version 1: for loop
+// Date: 1/29/2018
 function convertHTML(str) {
   let targets = /[&<>"']/;
   let pairs = {
