226b95cade8d7455b476788a77a4c446347ceb76
Brigitte Santos <santosbrigitte@gmail.com>
2018-02-10 16:56:38 -0800

    Add second solution for binary-agents.js

--- intermediate-algorithm-scripting/binary-agents.js
+++ intermediate-algorithm-scripting/binary-agents.js
@@ -1,4 +1,21 @@
 // Return an English translated sentence of the passed binary string.
+
+// Version 2: map
+// Date: 2/10/2018
+function binaryAgent(str) {
+  let values = [128,64,32,16,8,4,2,1];
+  
+  return str.split(' ').map(binary => {
+    let sum = 0;
+    for(let i=0; i<binary.length; i++){
+      if(binary[i] === '1'){ sum += values[i]; }
+    }
+    return String.fromCharCode(sum);
+  }).join('');
+}
+
+// Version 1: nested for loops
+// Date: 2/10/2018
 function binaryAgent(str) {
   let arr = str.split(' ');
   let values = [128,64,32,16,8,4,2,1];
